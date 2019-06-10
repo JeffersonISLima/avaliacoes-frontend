40f43f77c64a9146112174852ca933668900a49e
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-06-10 19:18:39 -0300

    Search bar done.

--- angular/src/app/search/search.page.ts
+++ angular/src/app/search/search.page.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
+import { legends } from '../names/nameslist';
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss']
 })
+
 export class SearchPage {
   searchInput : string;
   results = [];
-
+  legendsList: Array<string> = legends;
+  
   ngOnInit() {
+   
   }
 
   onSearch() {
-    console.log(`Search: ${this.searchInput}`)
-    
+    this.results = this.legendsList.filter( ( legend ) => {
+      return legend.toLowerCase().includes(this.searchInput.toLowerCase());
+    });
   }
 }
