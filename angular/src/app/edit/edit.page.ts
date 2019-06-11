cd897fc8801dda916e8d35235861612bfdb72c3e
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-06-11 13:11:07 -0300

    Edit page is done, but the challenge was not done. End of the Angular session.

--- angular/src/app/edit/edit.page.ts
+++ angular/src/app/edit/edit.page.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
+import { legends } from '../names/nameslist';
 
 @Component({
-  selector: 'app-edit',
+  selector: 'app-edit', 
   templateUrl: 'edit.page.html',
   styleUrls: ['edit.page.scss']
 })
-export class EditPage {
 
-  constructor() {
-    
+export class EditPage {
+  legendsList: Array<string> = legends;
+  editing = false; 
+  
+  ngOnInit() {
+    this.handleChange;
+  }
+
+  handleChange(event: any) {   
+    const { name, value } = event.target;    
+    const indexLegend = this.legendsList.indexOf(name); 
+    this.legendsList.splice(indexLegend, 1, value);
+    this.toggleView();
+  } 
+  
+  toggleView() {
+    this.editing = !this.editing;
   }
 
 }
