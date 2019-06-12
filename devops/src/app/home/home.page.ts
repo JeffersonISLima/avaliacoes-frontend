f7996ca33999e4db3c95957779fc44224cc66f47
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-06-11 21:33:27 -0300

    Devops: Done, implemented the Axios library to communicate with the API. Used Lodash to shuffle the array.

--- devops/src/app/home/home.page.ts
+++ devops/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { LoadingControlle } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+import  axios from 'axios'; 
 import * as _ from 'lodash';
 
 @Component({
@@ -7,6 +8,7 @@ import * as _ from 'lodash';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   loading;
   photos: Array<string> = [];
@@ -17,33 +19,28 @@ export class HomePage {
 
   }
 
-  ngOnInit() {
-    this.printOnConsole();
+  ngOnInit() {   
     this.presentLoading();
-
-
     this.randomPhotos();
 
     setTimeout( () => {
       try {
-        dismissLoading();
+        this.dismissLoading();
+       
       } catch(err) {
         console.error(err);
         alert('Error dismissing loader');
       }
-      
-    }, 5000)
-    
+    }, 1000)
   }
-
+  
   randomPhotos() {
-    let i = 0;
-    while (i < 501) {
-      let salt = Math.floor((Math.random() * 300) + 1);
-      this.photos.push(`https://picsum.photos/200/200?image=${salt}`)
-      i = i + 1;
-    }
-  }
+    axios.get(`https://picsum.photos/v2/list?page=2&limit=300`)
+      .then( ( response ) => {
+        this.photos = response.data;
+        this.photos =  _.shuffle(this.photos);
+    });  
+  }  
 
   async presentLoading() {
     this.loading = await this.loadingController.create({
@@ -52,11 +49,7 @@ export class HomePage {
     });
     return await this.loading.present();
   }
-
-  printOnConsole() {
-    console.log('Running a public function on init');
-  }
-
+  
   dismissLoading() {
     if (!_.isNil(this.loading)) {
       this.loading.dismiss();
